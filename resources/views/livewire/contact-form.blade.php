6bbc69ea688e8dc074d3bd8527f5109cd443812c
Hassan Agmir <hagmir6@gmail.com>
2025-01-20 21:55:59 +0100

    Update contact form style

--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -7,8 +7,7 @@
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
         <div>
             <label class="sr-only" for="full_name">Nom et Prénom</label>
-            <input wire:model='full_name' class="w-full p-3 text-sm border-gray-200 rounded-lg"
-                placeholder="Nom et Prénom" type="text" id="full_name" full_name="name">
+            <input wire:model='full_name' class="input-primary" placeholder="Nom et Prénom" type="text" id="full_name" full_name="name">
             <!-- Error area -->
             @error('full_name')
             <span class="text-white text-sm">{{ $message }}</span>
@@ -16,7 +15,7 @@
         </div>
         <div>
             <label class="sr-only" for="email">Adresse e-mail</label>
-            <input wire:model='email' class="w-full p-3 text-sm border-gray-200 rounded-lg" placeholder="Adresse e-mail"
+            <input wire:model='email' class="input-primary" placeholder="Adresse e-mail"
                 type="email" id="email" name="email">
             <!-- Error area -->
             @error('email')
@@ -28,9 +27,7 @@
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
         <div>
             <label class="sr-only" for="phone">Téléphone</label>
-            <input wire:model='phone' class="w-full p-3 text-sm border-gray-200 rounded-lg"
-                placeholder="Numéro de téléphone" type="tel" id="phone" name="phone">
-            <!-- Error area -->
+            <input wire:model='phone' class="input-primary" placeholder="Numéro de téléphone" type="tel" id="phone" name="phone">
             @error('phone')
             <span class="text-white text-sm">{{ $message }}</span>
             @enderror
@@ -38,9 +35,7 @@
 
         <div>
             <label class="sr-only" for="subject">Sujet</label>
-            <input wire:model='subject' class="w-full p-3 text-sm border-gray-200 rounded-lg" placeholder="Sujet"
-                type="text" id="subject" name="subject">
-            <!-- Error area -->
+            <input wire:model='subject' class="input-primary" placeholder="Sujet" type="text" id="subject" name="subject">
             @error('subject')
             <span class="text-white text-sm">{{ $message }}</span>
             @enderror
@@ -49,8 +44,7 @@
 
     <div>
         <label class="sr-only" for="message">Message</label>
-        <textarea wire:model='message' class="w-full p-3 text-sm border-gray-200 rounded-lg" placeholder="Message"
-            rows="8" id="message" name="message"></textarea>
+        <textarea wire:model='message' class="input-primary" placeholder="Message" rows="8" id="message" name="message"></textarea>
         <!-- Error area -->
         @error('message')
         <span class="text-white text-sm">{{ $message }}</span>
@@ -60,18 +54,19 @@
 
 
     <div class="mt-4">
-        <button type="button"
-            class="flex items-center justify-center gap-2 text-accent-red bg-white px-6 py-3 rounded-xl font-semibold hover:bg-accent-red hover:bg-transparent border hover:border-white hover:text-white"
-            wire:click="store" label="store">
-
-            <svg wire:loading xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path wire:loading.class="animate-rotate" d="M3 12a9 9 0 0 0 9 9a9 9 0 0 0 9 -9a9 9 0 0 0 -9 -9" />
-                <path wire:loading.class="animate-rotate-reverse" d="M17 12a5 5 0 1 0 -5 5" />
-            </svg>
-
-            Envoyer
-        </button>
+        <div class="mt-3">
+            <button type="submit" class="w-full btn btn-primary rounded-lg">
+                <svg wire:loading aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-red animate-spin"
+                    viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="#E5E7EB"></path>
+                    <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentColor"></path>
+                </svg>
+                {{ __("Send message") }}
+            </button>
+        </div>
     </div>
 </form>
