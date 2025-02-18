832d9e720802cc3a7b5e4d12375a397e457a9cf7
Hassan Agmir <hagmir6@gmail.com>
2025-02-18 21:16:21 +0100

    Update Category layoute

--- resources/views/livewire/category-list.blade.php
+++ resources/views/livewire/category-list.blade.php
@@ -1,28 +1,33 @@
 <section class="max-w-6xl mx-auto px-4">
-    {{-- <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-        @foreach ($categories as $category)
-        <x-category.defaul image="{{ Storage::url($category->image) }}" name="{{ $category->name }}"
-            url="{{ route('category.show', $category->slug) }}" products="{{ $category->products->count() }}"
-            description="{{ $category->description }}" />
-        @endforeach
-    </div> --}}
+    @switch(app('site')->options['category_layout'])
+        @case(1)
+            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                @foreach ($categories as $category)
+                <x-category.defaul image="{{ Storage::url($category->image) }}" name="{{ $category->name }}"
+                    url="{{ route('category.show', $category->slug) }}" products="{{ $category->products->count() }}"
+                    description="{{ $category->description }}" />
+                @endforeach
+            </div>
+            @break
 
+        @case(2)
+            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+                <!-- Headphones Category -->
+                @foreach ($categories as $category)
+                <x-category.primary image="{{ Storage::url($category->image) }}" name="{{ $category->name }}"
+                    url="{{ route('category.show', $category->slug) }}" products="{{ $category->products->count() }}"
+                    description="{{ $category->description }}" />
 
-    {{-- <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-        <!-- Headphones Category -->
-        @foreach ($categories as $category)
-        <x-category.primary image="{{ Storage::url($category->image) }}" name="{{ $category->name }}"
-            url="{{ route('category.show', $category->slug) }}" products="{{ $category->products->count() }}"
-            description="{{ $category->description }}" />
-
-        @endforeach
-    </div> --}}
-
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
-        @foreach ($categories as $category)
-        <x-category.secondary image="{{ Storage::url($category->image) }}" name="{{ $category->name }}"
-            url="{{ route('category.show', $category->slug) }}" products="{{ $category->products->count() }}"
-            description="{{ $category->description }}" />
-        @endforeach
-    </div>
+                @endforeach
+            </div>
+            @break
+        @default
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
+                @foreach ($categories as $category)
+                <x-category.secondary image="{{ Storage::url($category->image) }}" name="{{ $category->name }}"
+                    url="{{ route('category.show', $category->slug) }}" products="{{ $category->products->count() }}"
+                    description="{{ $category->description }}" />
+                @endforeach
+            </div>
+    @endswitch
 </section>
--- resources/views/livewire/home-category.blade.php
+++ resources/views/livewire/home-category.blade.php
@@ -1,40 +1,9 @@
-<section class="max-w-6xl mx-auto px-4 pt-6 mb-4">
+<section class="max-w-6xl mx-auto pt-6 mb-4">
     @if ($categories->count())
-        <h1 class="text-xl font-bold text-gray-900 mb-2">{{ __("Categories list") }}</h1>
+        {{-- <h1 class="text-xl font-bold text-gray-900 mb-2 px-4">{{ __("Categories list") }}</h1> --}}
 
-            {{-- <p class="text-gray-600 mb-4">
-                Explore Floor Warehouse's premium selection of Solid & Engineered Wood, Luxury Vinyl Tiles, and Laminate
-                Flooring, sourced from top suppliers to ensure quality and innovation at unbeatable prices. Discover your
-                perfect floor and order free samples today!
-            </p> --}}
-
-            {{-- <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-                @foreach ($categories as $category)
-                <x-category.defaul image="{{ Storage::url($category->image) }}" name="{{ $category->name }}"
-                    url="{{ route('category.show', $category->slug) }}" products="{{ $category->products->count() }}"
-                    description="{{ $category->description }}" />
-                @endforeach
-            </div> --}}
-
-            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-                <!-- Headphones Category -->
-                @foreach ($categories as $category)
-                <x-category.primary image="{{ Storage::url($category->image) }}" name="{{ $category->name }}"
-                    url="{{ route('category.show', $category->slug) }}" products="{{ $category->products->count() }}"
-                    description="{{ $category->description }}" />
-
-                @endforeach
-            </div>
-
-            {{-- <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                @foreach ($categories as $category)
-                <x-category.secondary image="{{ Storage::url($category->image) }}" name="{{ $category->name }}"
-                    url="{{ route('category.show', $category->slug) }}" products="{{ $category->products->count() }}"
-                    description="{{ $category->description }}" />
-                @endforeach
-            </div> --}}
+        @livewire('category-list')
     @endif
-
 </section>
 
 
