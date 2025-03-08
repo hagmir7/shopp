d3ed7558e1b2aca825ccc029899116783aba2e5a
Hassan Agmir <hagmir6@gmail.com>
2025-03-08 13:33:49 +0000

    Fix perforemance issu

--- resources/views/components/product-showcase.blade.php
+++ resources/views/components/product-showcase.blade.php
@@ -1,12 +1,13 @@
 <div class="thumbnail_slider">
     <!-- Primary Slider -->
-    <div id="primary_slider" class="splide" aria-live="polite">
+    <div id="primary_slider" class="splide">
         <!-- Added splide class -->
         <div class="splide__track">
-            <ul class="splide__list" role="menu">
+            <ul class="splide__list">
                 @foreach ($images as $image)
-                <li class="splide__slide" role="none">
-                    <img class="h-auto" role="menuitem" src="{{ Storage::url($image->path) }}" alt="{{ $name }} - Image {{ $loop->iteration }}">
+                <li class="splide__slide">
+                    <img class="h-auto" src="{{ Storage::url($image->path) }}"
+                        alt="{{ $name }} - Image {{ $loop->iteration }}">
                 </li>
                 @endforeach
             </ul>
@@ -14,14 +15,15 @@
     </div>
 
     <!-- Thumbnail Slider -->
-    <div id="thumbnail_slider" class="splide" aria-label="Thumbnail Slider">
+    <div id="thumbnail_slider" class="splide">
         @if ($images->count() > 1)
         <!-- Added splide class -->
         <div class="splide__track">
-            <ul class="splide__list" role="menu">
+            <ul class="splide__list">
                 @foreach ($images as $image)
-                <li class="splide__slide border border-1 border-gray-500" role="none">
-                    <img class="object-contain" style="border: 1.5px solid #8080804f;" src="{{ Storage::url($image->path) }}" alt="{{ $name }} - Thumbnail {{ $loop->iteration }}" role="menuitem">
+                <li class="splide__slide border border-1 border-gray-500">
+                    <img class="object-contain" style="border: 1.5px solid #8080804f;"
+                        src="{{ Storage::url($image->path) }}" alt="{{ $name }} - Thumbnail {{ $loop->iteration }}">
                 </li>
                 @endforeach
             </ul>
