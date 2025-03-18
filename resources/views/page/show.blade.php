c85a86cbac0ac692612cf48cf6c5d3bdb5d55a80
Hassan Agmir <hagmir6@gmail.com>
2025-03-18 23:04:09 +0000

    Add Tailwind Prose Typografy

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -4,7 +4,7 @@
 <div class="max-w-3xl mx-auto mt-3 px-2 mb-9">
     <h1 class="text-xl md:text-2xl py-4 font-bold">{{ $page->title }}</h1>
     <p class="mb-3 font-bold">{{ __("Last updated") }} {{ $page->updated_at }}</p>
-    <div class="mb-4">
+    <div class="mb-4 prose prose:text-lg">
         {!! $page->content !!}
     </div>
 </div>
